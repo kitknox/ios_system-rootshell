d26afc264a78723afba6286695eadba5184dd7af
Kit Knox <kit@rootshell.com>
2026-09-22 10:52:55 -0700

    Release 0.1.1

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.2
 
 import PackageDescription
 
 let package = Package(
     name: "ios_system",
     platforms: [
-        .iOS(.v14),
-        .macCatalyst(.v14),
-        .visionOS(.v1),
+        .iOS(.v18),
+        .macCatalyst(.v18),
+        .visionOS(.v26),
     ],
     products: [
         .library(
@@ -18,28 +18,28 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ios_system",
-            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.0/ios_system.xcframework.zip",
-            checksum: "ce6a816ab9a901fe5df31ad6d819e19b773db32d25f612aa8318da11c6bc2cba"
+            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.1/ios_system.xcframework.zip",
+            checksum: "a2ba0979ae598ad6502b0646ebb4030c05494f900597db76a23f6693a4c85726"
         ),
         .binaryTarget(
             name: "awk",
-            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.0/awk.xcframework.zip",
-            checksum: "4e65e4f31a1a6b9270de0b7d2bb8514933c4693686f9659beb35db51043c324e"
+            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.1/awk.xcframework.zip",
+            checksum: "3c521da24fcfd4b42d7589394c8810ca25f960de2857e128ef7a9078a6aa12bf"
         ),
         .binaryTarget(
             name: "files",
-            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.0/files.xcframework.zip",
-            checksum: "a55e031b73974e94b209d43343e0608a188baf887f91d45eb7a7f9d112197c90"
+            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.1/files.xcframework.zip",
+            checksum: "c75548c67c3e1349269011a933815385a25b38b5cb4ae31e5ca372a35045191c"
         ),
         .binaryTarget(
             name: "shell",
-            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.0/shell.xcframework.zip",
-            checksum: "7d6c39a0c5ca8bedef3c2ba97ebf8be01b0c62f3960feaa16a1a9fcb5cba2aa6"
+            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.1/shell.xcframework.zip",
+            checksum: "8582d18805d1d6ab6f650ec41fda404073234904344020f5dfa48b2e73ab7bec"
         ),
         .binaryTarget(
             name: "text",
-            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.0/text.xcframework.zip",
-            checksum: "8a3bb33c303ff3c51c3ddb23748bd989afc24d88353153dd7484e0a561ea3f11"
+            url: "https://github.com/kitknox/ios_system-rootshell/releases/download/v0.1.1/text.xcframework.zip",
+            checksum: "accf13b497593b228abf28c54a2f79d4c61ec6eb825a9dc13ce13c8e84c7ac65"
         ),
     ]
 )
